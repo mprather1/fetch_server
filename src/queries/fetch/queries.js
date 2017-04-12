e33a0e1b992bb500203a36eebdcd2988482f3008
Michael Prather <mprather1@gmail.com>
2017-04-12 20:19:16 +0000

    post request works; needs to be refactored

--- src/queries/fetch/queries.js
+++ src/queries/fetch/queries.js
@@ -4,10 +4,10 @@ import request from 'request'
 
 function fetchAllModels (req, res, options) {
   const {serverAddress, logger} = options
-  let results
+
   const address = url.resolve(serverAddress, '/api/models')
   
-  requestp(address, true).then((data) => {
+  requestp(address, true, 'GET', null).then((data) => {
     res.status(200)
     .json(data)
   })
@@ -17,36 +17,52 @@ function fetchAllModels (req, res, options) {
   })
 }
 
-function requestp(url, json) {
-  json = json || false
-  return new Promise(function (resolve, reject) {
-    request({url:url, json:json}, function (err, res, body) {
-      if (err) {
-        console.log(err)
-        return reject(err)
-      }
-      if (res.statusCode !== 200) {
-        err = new Error('Unexpected status code: ' + res.statusCode)
-        return reject(err)
-      }
-      resolve(body)
-    })
+function createModel(req, res, options) {
+  const {serverAddress, logger} = options
+
+  const address = url.resolve(serverAddress, '/api/models')
+  
+  requestp(address, true, 'POST', req.body).then((data) => {
+    res.status(200)
+    .json(data)
   })
+  
+  .catch((err) => {
+    logger.error(err)
+  })  
 }
 
-async function createModel (req, res, options) {
-  const {serverAddress} = options
-  const address = url.resolve(serverAddress, '/api/models')
-
-  const {body} = await got.post(address, {
-    json: true,
-    body: req.body,
-    timeout: 15000
-  })
-  res.status(200)
-  .json({
-    status: 'success'
-  })
+function requestp(url, json, method, requestBody) {
+  json = json || false
+  if (requestBody === null) {
+    return new Promise(function (resolve, reject) {
+      request({url:url, json:json, method: method}, function (err, res, body) {
+        if (err) {
+          console.log(err)
+          return reject(err)
+        }
+        if (res.statusCode !== 200) {
+          err = new Error('Unexpected status code: ' + res.statusCode)
+          return reject(err)
+        }
+        resolve(body)
+      })
+    })    
+  } else {
+    return new Promise(function (resolve, reject) {
+      request({url:url, json:json, method: method, body:requestBody}, function (err, res, body) {
+        if (err) {
+          console.log(err)
+          return reject(err)
+        }
+        if (res.statusCode !== 200) {
+          err = new Error('Unexpected status code: ' + res.statusCode)
+          return reject(err)
+        }
+        resolve(body)
+      })
+    })      
+  }
 }
 
 export default {
